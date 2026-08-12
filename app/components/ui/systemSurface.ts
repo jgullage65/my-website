8bb4c68180fe65fec4ff0afc0fcae6a337d62aa9
James Gullage <jgullage65@gmail.com>
2026-08-11 21:51:25 -0500

    Add canonical System surface classes

---
+++ app/components/ui/systemSurface.ts
@@ -0,0 +1,7 @@
+export const SYSTEM_SURFACE_CLASS = "bg-[#090C11]";
+
+export const SYSTEM_CARD_CLASS =
+  "rounded-xl border border-white/[0.12] bg-[#090C11] shadow-[0_8px_18px_rgba(0,0,0,0.38),inset_0_1px_0_rgba(255,255,255,0.05),inset_0_-1px_0_rgba(0,0,0,0.72)]";
+
+export const SYSTEM_CARD_INTERACTIVE_CLASS =
+  `${SYSTEM_CARD_CLASS} transition-[background-color,border-color,box-shadow] duration-150 hover:border-white/[0.15] hover:bg-[#070A0F] hover:shadow-[0_10px_22px_rgba(0,0,0,0.42),inset_0_1px_0_rgba(255,255,255,0.06),inset_0_-1px_0_rgba(0,0,0,0.76)]`;
